303ed64d9d5f4eb3b489cd4642f86731431af0de
Zane Bookbinder <zanemb86@gmail.com>
2023-01-23 17:25:59 -0500

    Page not found

--- personal-website/src/components/PageNotFound.js
+++ personal-website/src/components/PageNotFound.js
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
+import "./PageNotFound.css"
 
 function PageNotFound() {
 	return (
-		<div style={{marginLeft: "5rem", marginTop: "1rem"}}>
-			<h1 style={{marginBottom: "30px", fontSize: "28px"}}>Sorry, this page does not exist.</h1>
-			<Link to="/" style={{borderRadius: "30px", marginTop: "40px", border: "1px solid var(--primary-text)", padding: "10px 15px", fontSize: "20px"}}>Return home</Link>
+		<div className="page-not-found">
+			<h1 className="page-not-found-title">Sorry, this page does not exist.</h1>
+			<Link to="/" className="return-home-button">Return home</Link>
 		</div>
 	);
 }
