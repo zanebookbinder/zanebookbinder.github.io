a72e6fde26905ee9240a3741ebb206763073d988
Zane Bookbinder <zanemb86@gmail.com>
2023-09-06 13:59:05 -0400

    Timeline change

--- personal-website/src/components/HomePage/HomePage.js
+++ personal-website/src/components/HomePage/HomePage.js
@@ -10,13 +10,12 @@ import BcrMobileStatsPage from "../../images/bcr-mobile-statspage.png";
 import BcrMobileProfilePage from "../../images/bcr-mobile-profilepage.png";
 
 function HomePage() {
-    
-	const quickLinks = (
+    const quickLinks = (
         <>
             <p className="small-text homePageText">
-                I'm a rising senior at Bowdoin College majoring in Computer Science and
-                minoring in Economics. I'm really passionate about software,
-                data, and AI. This summer, I worked as a Software
+                I'm a rising senior at Bowdoin College majoring in Computer
+                Science and minoring in Economics. I'm really passionate about
+                software, data, and AI. This summer, I worked as a Software
                 Engineering Intern at Amazon (if you're from Seattle, send me a
                 message!). Please check out my bio, projects, and resume. I
                 would love to connect!
@@ -57,7 +56,7 @@ function HomePage() {
                     <h1 className="header nontyped-title">Hi, I'm Zane!</h1>
                     {quickLinks}
                 </div>
-				<img className="zane-image" src={ZaneImage} alt="Zane" />
+                <img className="zane-image" src={ZaneImage} alt="Zane" />
             </div>
             <div className="mobile-top-section">
                 <div className="mobile-top">
@@ -77,8 +76,11 @@ function HomePage() {
                         src={BcrCoursePage}
                     />
                 </div>
-				<div className="bcr-aboutme-text">
-                    <h1 id="working-on-now" className="working-on-now-title homepage-title">
+                <div className="bcr-aboutme-text">
+                    <h1
+                        id="working-on-now"
+                        className="working-on-now-title homepage-title"
+                    >
                         Bowdoin Course Reviews
                     </h1>
                     <p className="small-text homePageText">
@@ -93,108 +95,126 @@ function HomePage() {
                         >
                             Olivia
                         </a>
-                        ! Every registration period, we heard friends and classmates 
-						asking “Have you taken this class? How was it?” or “Have you 
-						ever had Professor so and so? I've heard she's pretty tough.” 
-						So, we decided to give students a centralized and reliable place to
-						shout out their favorite professors or warn others about tough 
-						courses they took. We plan
-						to launch the site in the fall of 2023, and we hope that it 
-						will help students make well-informed decisions about course
-						registration.
+                        ! Every registration period, we heard friends and
+                        classmates asking “Have you taken this class? How was
+                        it?” or “Have you ever had Professor so and so? I've
+                        heard she's pretty tough.” So, we decided to give
+                        students a centralized and reliable place to shout out
+                        their favorite professors or warn others about tough
+                        courses they took. We plan to launch the site in the
+                        fall of 2023, and we hope that it will help students
+                        make well-informed decisions about course registration.
                     </p>
                 </div>
             </div>
-			<div className="bcr-timeline-div">
-				<div className="bcr-timeline-left">
-					<h1 className="homepage-title">Our Development Timeline</h1>
-					<ul className="bcr-timeline">
-						<li className="bcr-timeline-item">
-							<h3 className="bcr-timeline-title">December 2022</h3>
-							<p className="bcr-timeline-text">
-								Olivia and I began planning out the website's architecture and
-								basic functionality. For the site's tech stack, we chose a MySQL
-								backend, a Flask API, and a React frontend.
-							</p>
-						</li>
-						<li className="bcr-timeline-item left-arrow-icon">
-							<h3 className="bcr-timeline-title">January 2023</h3>
-							<p className="bcr-timeline-text">
-								We migrated the database from the 2012 version of Bowdoin
-								Course Reviews and hosted it on AWS RDS. We then began
-								working on the API and frontend. We created a home page,
-								course pages, professor pages, and department pages.
-							</p>
-						</li>
-						<li className="bcr-timeline-item">
-							<h3 className="bcr-timeline-title">February and March 2023</h3>
-							<p className="bcr-timeline-text">
-								We added a login system, a create review page, and a user profile page.
-								With the help of Professor Stephen Houser, we also moved the API and
-								front end to AWS App Runner.
-							</p>
-						</li>
-						<li className="bcr-timeline-item left-arrow-icon">
-							<h3 className="bcr-timeline-title">April 2023</h3>
-							<p className="bcr-timeline-text">
-								We started adding extra features: a stats page, the ability to
-								sort and filter review data, and smooth loading animations.
-							</p>
-						</li>
-						<li className="bcr-timeline-item">
-							<h3 className="bcr-timeline-title">May 2023</h3>
-							<p className="bcr-timeline-text">
-								We improved the site's UI by making it fully responsive and fixing
-								visual bugs. We asked a small group of family and friends to test
-								the site and give us feedback.
-							</p>
-						</li>
-						<li className="bcr-timeline-item">
-							<h3 className="bcr-timeline-title">What's next</h3>
-							<p className="bcr-timeline-text">
-								The site is nearly done! This summer, we'll work with Bowdoin's
-								IT Department to connect the login system to Okta, which will
-								allow students to log in with their Bowdoin credentials.
-							</p>
-						</li>
-						<li className="bcr-timeline-item">
-							<h3 className="bcr-timeline-title">September 2023</h3>
-							<p className="bcr-timeline-text">
-								We plan to launch the site during the first week of the fall semester.
-							</p>
-						</li>
-					</ul>
-				</div>
-				<div className="bcr-timeline-images">
-					<img
-						className="bcr-department-image"
-						alt="Bowdoin Course Reviews Department Page"
-						src={BcrDepartmentPage}
-					/>
-					<img
-						className="bcr-createreview-image"
-						alt="Bowdoin Course Reviews Create Review Page"
-						src={BcrCreateReviewPage}
-					/>
-					<div style={{display: "flex", justifyContent: "space-around"}}>
-						<img
-							className="bcr-mobile-homepage-image"
-							alt="Bowdoin Course Reviews Home Page"
-							src={BcrMobileHomePage}
-						/>
-						<img
-							className="bcr-mobile-stats-image"
-							alt="Bowdoin Course Reviews Stats Page"
-							src={BcrMobileStatsPage}
-						/>
-						<img
-							className="bcr-mobile-profile-image"
-							alt="Bowdoin Course Reviews Profile Page"
-							src={BcrMobileProfilePage}
-						/>
-					</div>
-				</div>
-			</div>
+            <div className="bcr-timeline-div">
+                <div className="bcr-timeline-left">
+                    <h1 className="homepage-title">Our Development Timeline</h1>
+                    <ul className="bcr-timeline">
+                        <li className="bcr-timeline-item">
+                            <h3 className="bcr-timeline-title">
+                                December 2022
+                            </h3>
+                            <p className="bcr-timeline-text">
+                                Olivia and I began planning out the website's
+                                architecture and basic functionality. For the
+                                site's tech stack, we chose a MySQL backend, a
+                                Flask API, and a React frontend.
+                            </p>
+                        </li>
+                        <li className="bcr-timeline-item left-arrow-icon">
+                            <h3 className="bcr-timeline-title">January 2023</h3>
+                            <p className="bcr-timeline-text">
+                                We migrated the database from the 2012 version
+                                of Bowdoin Course Reviews and hosted it on AWS
+                                RDS. We then began working on the API and
+                                frontend. We created a home page, course pages,
+                                professor pages, and department pages.
+                            </p>
+                        </li>
+                        <li className="bcr-timeline-item">
+                            <h3 className="bcr-timeline-title">
+                                February and March 2023
+                            </h3>
+                            <p className="bcr-timeline-text">
+                                We added a login system, a create review page,
+                                and a user profile page. With the help of
+                                Professor Stephen Houser, we also moved the API
+                                and front end to AWS App Runner.
+                            </p>
+                        </li>
+                        <li className="bcr-timeline-item left-arrow-icon">
+                            <h3 className="bcr-timeline-title">April 2023</h3>
+                            <p className="bcr-timeline-text">
+                                We started adding extra features: a stats page,
+                                the ability to sort and filter review data, and
+                                smooth loading animations.
+                            </p>
+                        </li>
+                        <li className="bcr-timeline-item">
+                            <h3 className="bcr-timeline-title">May 2023</h3>
+                            <p className="bcr-timeline-text">
+                                We improved the site's UI by making it fully
+                                responsive and fixing visual bugs. We asked a
+                                small group of family and friends to test the
+                                site and give us feedback.
+                            </p>
+                        </li>
+                        <li className="bcr-timeline-item">
+                            <h3 className="bcr-timeline-title">What's next</h3>
+                            <p className="bcr-timeline-text">
+                                The site is nearly done! This summer, we'll work
+                                with Bowdoin's IT Department to connect the
+                                login system to Okta, which will allow students
+                                to log in with their Bowdoin credentials.
+                            </p>
+                        </li>
+                        <li className="bcr-timeline-item">
+                            <h3 className="bcr-timeline-title">
+                                September 2023
+                            </h3>
+                            <p className="bcr-timeline-text">
+                                We plan to launch the site during the middle of
+                                the fall semester.
+                            </p>
+                        </li>
+                    </ul>
+                </div>
+                <div className="bcr-timeline-images">
+                    <img
+                        className="bcr-department-image"
+                        alt="Bowdoin Course Reviews Department Page"
+                        src={BcrDepartmentPage}
+                    />
+                    <img
+                        className="bcr-createreview-image"
+                        alt="Bowdoin Course Reviews Create Review Page"
+                        src={BcrCreateReviewPage}
+                    />
+                    <div
+                        style={{
+                            display: "flex",
+                            justifyContent: "space-around",
+                        }}
+                    >
+                        <img
+                            className="bcr-mobile-homepage-image"
+                            alt="Bowdoin Course Reviews Home Page"
+                            src={BcrMobileHomePage}
+                        />
+                        <img
+                            className="bcr-mobile-stats-image"
+                            alt="Bowdoin Course Reviews Stats Page"
+                            src={BcrMobileStatsPage}
+                        />
+                        <img
+                            className="bcr-mobile-profile-image"
+                            alt="Bowdoin Course Reviews Profile Page"
+                            src={BcrMobileProfilePage}
+                        />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
