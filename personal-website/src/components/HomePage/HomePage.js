fe492279b19658fc6ace6e04ac5c2f2cd269a4fe
owirsching <olivia.wirsching@gmail.com>
2024-01-12 09:10:25 -0500

    Liv's Style Changes

--- personal-website/src/components/HomePage/HomePage.js
+++ personal-website/src/components/HomePage/HomePage.js
@@ -1,5 +1,6 @@
 import "./HomePage.css";
 import ZaneImage from "../../images/Zane-3.jpg";
+import BCRImage from "../../images/BCR.png";
 import { Link } from "react-router-dom";
 import Typed from "react-typed";
 import BcrCoursePage from "../../images/bcr-coursepage.png";
@@ -21,19 +22,19 @@ function HomePage() {
                 so feel free to take a look! Please check out my bio, projects, and resume. I
                 would love to connect!
             </p>
-            <p className="quicklink-title">Check out my:</p>
+            <p className="quicklink-title">CHECK OUT MY</p>
             <div className="quicklinks">
                 <Link to="/experience" state={{ scrollTo: "amazon" }}>
-                    2023 internship
+                    2023 Internship
                 </Link>
                 <Link to="/aboutme" state={{ scrollTo: "skills" }}>
-                    Technical skills
+                    Technical Skills
                 </Link>
                 <Link to="/experience" state={{ scrollTo: "datto" }}>
-                    Previous internship
+                    2022 Internship
                 </Link>
                 <Link to="/experience" state={{ scrollTo: "GPS" }}>
-                    Favorite past project
+                    Favorite Project
                 </Link>
             </div>
         </>
@@ -69,15 +70,10 @@ function HomePage() {
                 </div>
                 {quickLinks}
             </div>
+            
             <div className="bcr-homepage">
-                <div className="bcr-screenshot-div">
-                    <img
-                        className="bcr-screenshot"
-                        alt="Bowdoin Course Reviews"
-                        src={BcrCoursePage}
-                    />
-                </div>
                 <div className="bcr-aboutme-text">
+                    <div className="tag quicklink-title">RECENT PROJECT</div>
                     <h1
                         id="working-on-now"
                         className="working-on-now-title homepage-title"
@@ -126,104 +122,111 @@ function HomePage() {
                         newspaper to learn more about the project and how it's helping students!
                     </p>
                 </div>
+                <img className="bcr-image" src={BCRImage} />
             </div>
             <div className="bcr-timeline-div">
                 <div className="bcr-timeline-left">
                     <h1 className="homepage-title">Our Development Timeline</h1>
                     <ul className="bcr-timeline">
-                        <li className="bcr-timeline-item">
-                            <h3 className="bcr-timeline-title">
-                                December 2022
-                            </h3>
-                            <p className="bcr-timeline-text">
-                                Olivia and I began planning out the website's
-                                architecture and basic functionality. For the
-                                site's tech stack, we chose a MySQL backend, a
-                                Flask API, and a React frontend.
-                            </p>
-                        </li>
-                        <li className="bcr-timeline-item left-arrow-icon">
-                            <h3 className="bcr-timeline-title">January 2023</h3>
-                            <p className="bcr-timeline-text">
-                                We migrated the database from the 2012 version
-                                of Bowdoin Course Reviews and hosted it on AWS
-                                RDS. We then began working on the API and
-                                frontend. We created a home page, course pages,
-                                professor pages, and department pages.
-                            </p>
-                        </li>
-                        <li className="bcr-timeline-item">
-                            <h3 className="bcr-timeline-title">
-                                February and March 2023
-                            </h3>
-                            <p className="bcr-timeline-text">
-                                We added a login system, a create review page,
-                                and a user profile page. With the help of
-                                Professor Stephen Houser, we also moved the API
-                                and front end to AWS App Runner.
-                            </p>
-                        </li>
-                        <li className="bcr-timeline-item left-arrow-icon">
-                            <h3 className="bcr-timeline-title">April 2023</h3>
-                            <p className="bcr-timeline-text">
-                                We started adding extra features: a stats page,
-                                the ability to sort and filter review data, and
-                                smooth loading animations.
-                            </p>
-                        </li>
-                        <li className="bcr-timeline-item">
-                            <h3 className="bcr-timeline-title">May 2023</h3>
-                            <p className="bcr-timeline-text">
-                                We improved the site's UI by making it fully
-                                responsive and fixing visual bugs. We asked a
-                                small group of family and friends to test the
-                                site and give us feedback.
-                            </p>
-                        </li>
-                        <li className="bcr-timeline-item">
-                            <h3 className="bcr-timeline-title">
-                                Summer 2023
-                            </h3>
-                            <p className="bcr-timeline-text">
-                                We made small bug fixes, iterated on our UI, and
-                                began working on a login system.
-                            </p>
-                        </li>
-                        <li className="bcr-timeline-item">
-                            <h3 className="bcr-timeline-title">
-                                September and October 2023
-                            </h3>
-                            <p className="bcr-timeline-text">
-                                We integrated our site with Okta for login and 
-                                session management! Bowdoin students can now login
-                                to the website with their school account.
-                            </p>
-                        </li>
-                        <li className="bcr-timeline-item">
-                            <h3 className="bcr-timeline-title">November 6, 2023</h3>
-                            <p className="bcr-timeline-text">
-                                We launched the site to Bowdoin's 1,900 students and
-                                received a ton of positive feedback, including that 
-                                the site "looks like a spaceship" and that using it 
-                                is a "very smooth software experience." In the first three 
-                                days after launch, we received 500 unique site visitors, 
-                                nearly 400 new reviews for courses, and over 1,600 home page 
-                                visits.
-                            </p>
-                        </li>
-                        <li className="bcr-timeline-item">
-                            <h3 className="bcr-timeline-title">What's next?</h3>
-                            <p className="bcr-timeline-text">
-                                We will fix any bugs that arise on the site and 
-                                continue to add features. One idea we'd like to implement is
-                                a 'Next Semester' page where people can plan their schedule 
-                                for the next semester by looking at class times and availability,
-                                all in one place.
-                            </p>
-                        </li>
+                        <div class="timeline-left">
+                            <li className="bcr-timeline-item">
+                                <h3 className="bcr-timeline-title">
+                                    December 2022
+                                </h3>
+                                <p className="bcr-timeline-text">
+                                    Olivia and I began planning out the website's
+                                    architecture and basic functionality. For the
+                                    site's tech stack, we chose a MySQL backend, a
+                                    Flask API, and a React frontend.
+                                </p>
+                            </li>
+                            <li className="bcr-timeline-item left-arrow-icon">
+                                <h3 className="bcr-timeline-title">January 2023</h3>
+                                <p className="bcr-timeline-text">
+                                    We migrated the database from the 2012 version
+                                    of Bowdoin Course Reviews and hosted it on AWS
+                                    RDS. We then began working on the API and
+                                    frontend. We created a home page, course pages,
+                                    professor pages, and department pages.
+                                </p>
+                            </li>
+                            <li className="bcr-timeline-item">
+                                <h3 className="bcr-timeline-title">
+                                    February and March 2023
+                                </h3>
+                                <p className="bcr-timeline-text">
+                                    We added a login system, a create review page,
+                                    and a user profile page. With the help of
+                                    Professor Stephen Houser, we also moved the API
+                                    and front end to AWS App Runner.
+                                </p>
+                            </li>
+                            <li className="bcr-timeline-item left-arrow-icon">
+                                <h3 className="bcr-timeline-title">April 2023</h3>
+                                <p className="bcr-timeline-text">
+                                    We started adding extra features: a stats page,
+                                    the ability to sort and filter review data, and
+                                    smooth loading animations.
+                                </p>
+                            </li>
+                        </div>
+                        <div class="timeline-right">
+                            <li className="bcr-timeline-item">
+                                <h3 className="bcr-timeline-title">May 2023</h3>
+                                <p className="bcr-timeline-text">
+                                    We improved the site's UI by making it fully
+                                    responsive and fixing visual bugs. We asked a
+                                    small group of family and friends to test the
+                                    site and give us feedback.
+                                </p>
+                            </li>
+                            <li className="bcr-timeline-item">
+                                <h3 className="bcr-timeline-title">
+                                    Summer 2023
+                                </h3>
+                                <p className="bcr-timeline-text">
+                                    We made small bug fixes, iterated on our UI, and
+                                    began working on a login system.
+                                </p>
+                            </li>
+                            <li className="bcr-timeline-item">
+                                <h3 className="bcr-timeline-title">
+                                    September and October 2023
+                                </h3>
+                                <p className="bcr-timeline-text">
+                                    We integrated our site with Okta for login and 
+                                    session management! Bowdoin students can now login
+                                    to the website with their school account.
+                                </p>
+                            </li>
+                            <li className="bcr-timeline-item">
+                                <h3 className="bcr-timeline-title">November 6, 2023</h3>
+                                <p className="bcr-timeline-text">
+                                    We launched the site to Bowdoin's 1,900 students and
+                                    received a ton of positive feedback, including that 
+                                    the site "looks like a spaceship" and that using it 
+                                    is a "very smooth software experience." In the first three 
+                                    days after launch, we received 500 unique site visitors, 
+                                    nearly 400 new reviews for courses, and over 1,600 home page 
+                                    visits.
+                                </p>
+                            </li>
+                            <li className="bcr-timeline-item">
+                                <h3 className="bcr-timeline-title">What's next?</h3>
+                                <p className="bcr-timeline-text">
+                                    We will fix any bugs that arise on the site and 
+                                    continue to add features. One idea we'd like to implement is
+                                    a 'Next Semester' page where people can plan their schedule 
+                                    for the next semester by looking at class times and availability,
+                                    all in one place.
+                                </p>
+                            </li>
+                        </div>
                     </ul>
                 </div>
-                <div className="bcr-timeline-images">
+                
+            </div>
+            <div className="bcr-timeline-images">
                     <img
                         className="bcr-department-image"
                         alt="Bowdoin Course Reviews Department Page"
@@ -254,7 +257,6 @@ function HomePage() {
                         />
                     </div>
                 </div>
-            </div>
         </div>
     );
 }
