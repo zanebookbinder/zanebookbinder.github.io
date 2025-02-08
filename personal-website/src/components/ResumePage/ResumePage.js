4b604ef9adf0b44077199f54c4249da15092e36f
Zane Bookbinder <zane.bookbinder@gmail.com>
2025-02-08 14:35:26 -0500

    Updated resume link

--- personal-website/src/components/ResumePage/ResumePage.js
+++ personal-website/src/components/ResumePage/ResumePage.js
@@ -1,44 +1,47 @@
-import "./ResumePage.css"
+import "./ResumePage.css";
 import { useEffect, useState } from "react";
 
 function ResumePage() {
+    const [resumeLoaded, setResumeLoaded] = useState(false);
 
-	const [resumeLoaded, setResumeLoaded] = useState(false);
+    const googleDriveResumeLink =
+        "https://drive.google.com/file/d/1KURXFVMHgnceYh2FhnUgWPfSh89tWe-h/";
 
-	const handleLoad = () => {
-		setResumeLoaded(true);
-	};
+    const handleLoad = () => {
+        setResumeLoaded(true);
+    };
 
-	useEffect(() => {
-		document.title = "Resume | Zane Bookbinder";
-	}, []);
+    useEffect(() => {
+        document.title = "Resume | Zane Bookbinder";
+    }, []);
 
-	return (
-	  <div className="resume-page">
-		<div className="resume-block">
-			<div>
-				{resumeLoaded &&
-					<div className="download">
-						<a 
-							className="here-link"
-							href="https://drive.google.com/file/d/1KURXFVMHgnceYh2FhnUgWPfSh89tWe-h/view" 
-							
-							target="_blank"
-							rel="noreferrer"
-							>Download a copy</a>
-					</div>
-				}
-				<iframe
-					className="resume-embed"
-					onLoad={handleLoad}
-					title="my-resume"
-					src="https://drive.google.com/file/d/1dOaSdJIIo3veSH1SMReiO7x7Xp20WTcQ/preview"
-					allow="autoplay"
-				></iframe>
-			</div>		
-		</div>
-	  </div>
-	);
-  }
-  
-  export default ResumePage;
+    return (
+        <div className="resume-page">
+            <div className="resume-block">
+                <div>
+                    {resumeLoaded && (
+                        <div className="download">
+                            <a
+                                className="here-link"
+                                href={googleDriveResumeLink + "view"}
+                                target="_blank"
+                                rel="noreferrer"
+                            >
+                                Download a copy
+                            </a>
+                        </div>
+                    )}
+                    <iframe
+                        className="resume-embed"
+                        onLoad={handleLoad}
+                        title="my-resume"
+                        src={googleDriveResumeLink + "preview"}
+                        allow="autoplay"
+                    ></iframe>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ResumePage;
